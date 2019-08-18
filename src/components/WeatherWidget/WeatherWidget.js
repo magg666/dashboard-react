412c1a06d0fac44416516fe21aabcc35ed351ddf
magg666 <mw23127@gmail.com>
2019-08-18 19:04:17 +0200

    prepared files for weather widget In progress

---
+++ src/components/WeatherWidget/WeatherWidget.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+// fetch data
+class WeatherWidget extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            longitude : '21.012230',
+            latitude: '52.229675',
+
+        }
+    }
+
+
+
+
+
+}
+
+
+// format data
+
+
+// render html
+
+
+
+
