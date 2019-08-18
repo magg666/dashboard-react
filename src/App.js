319f30d54ec020c0d0c825f985831632c19ed6f2
magg666 <mw23127@gmail.com>
2019-08-18 19:07:31 +0200

    main app files. Needs to be heavily refactored

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {ControlledCarousel} from "./components/carousel";
+import Clock from "./components/Clock/Clock";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        githubData: []
+    };
+
+    render() {
+        return (
+
+            <div>
+                <ControlledCarousel/>
+                <Clock/>
+            </div>
+        )
+    }
+
 }
 
 export default App;
