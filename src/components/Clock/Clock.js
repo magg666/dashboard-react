07928e95e401f95c0403ba491ce0770dd4d7f118
magg666 <mw23127@gmail.com>
2019-08-19 12:17:46 +0200

    Refactored, added documentation

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,29 +1,50 @@
-// https://tutorialscapital.com/react-native-create-live-digital-clock-using-local-system-time-android-ios-tutorial/
 import React, {Component} from "react";
 import {formatTime} from "./Clock_container";
 import './Clock_style.css'
 import {Captions} from "./Clock_container";
 
+/**
+ * Clock component with day name, time and captions with time (HH:MM) remaining to define event
+ * with help :  https://tutorialscapital.com/
+ *              react-native-create-live-digital-clock-using-
+ *              local-system-time-android-ios-tutorial/
+ */
+
 export default class Clock extends Component {
     constructor(props) {
         super(props);
+        /**
+         * initial state of clock
+         *
+         * @type {{currentTime: string,
+         * hour: *,
+         * currentDay: null,
+         * daysArray: *[],
+         * minutes: *,
+         * caption: string}}
+         */
         this.state = {
+
             hour: new Date().getHours(),
             minutes: new Date().getMinutes(),
-            seconds: new Date().getSeconds(),
             currentDay: null,
             daysArray: ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'],
             caption: "",
             currentTime: ""
         };
-
     }
 
+    /**
+     * Get current local time and sets state accordingly
+     */
     getCurrentTime() {
         this.setState({hour: new Date().getHours()});
         this.setState({minutes: new Date().getMinutes()});
-        this.setState({seconds: new Date().getSeconds()});
-        this.setState({currentTime: formatTime(this.state.hour, this.state.minutes, this.state.seconds)});
+
+        //returns time in human-readable format
+        this.setState({currentTime: formatTime(this.state.hour, this.state.minutes, new Date().getSeconds())});
+
+        // assigns day name from array to day number from Date
         // eslint-disable-next-line array-callback-return
         this.state.daysArray.map((item, index) => {
             if (index === new Date().getDay()) {
@@ -32,11 +53,16 @@ export default class Clock extends Component {
         })
     }
 
+    /**
+     * set state for appropriate caption
+     */
     getCaption() {
         this.setState({caption: new Captions(this.state.hour, this.state.minutes).getCurrentCaption()})
     }
 
-
+    /**
+     * On mounting component sets timer interval to refresh page by 1 second and by that simulate clock ticking
+     */
     componentDidMount() {
         this.timer = setInterval(() => {
             this.getCurrentTime();
@@ -44,10 +70,17 @@ export default class Clock extends Component {
         },1000)
     }
 
+    /**
+     * Before un-mounting timer is cleared
+     */
     componentWillUnmount() {
         clearInterval(this.timer)
     }
 
+    /**
+     * Renders html for clock
+     * @returns {*}
+     */
     render() {
         return (
             <div className="clock-container">
