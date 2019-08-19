b3fa83d497c02130b430eaa0aaefd89c991f68a9
magg666 <mw23127@gmail.com>
2019-08-19 18:31:41 +0200

    added documentation to Panel component

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+/**
+ * ComponentHOC - panel to wrap all widget
+ * @param WrappedComponent
+ * @returns {function(*): *}
+ * @constructor
+ */
 const PanelHoc = (WrappedComponent) => (props) => (
     <div className="container-fluid"><WrappedComponent {...props}/></div>
 );
-
 
 export default PanelHoc
