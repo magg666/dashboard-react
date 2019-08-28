875d4500a02e64418f0f961b9ec536c802fb00fb
magg666 <mw23127@gmail.com>
2019-08-28 22:46:32 +0200

    added documentation for numbers facts widget

--- src/components/NumberFactsWidget/NumberFactsWidget.js
+++ src/components/NumberFactsWidget/NumberFactsWidget.js
@@ -1,28 +1,38 @@
 import React, {useEffect, useState} from "react";
 
-
+/**
+ * Component to display random, interesting facts about numbers
+ * @returns {*}
+ * @constructor
+ */
 export const NumberFactWidget = () => {
+    // initial state
     let [number, setNumber] = useState("");
 
-
+    /**
+     * Fetches data from api about numbers.
+     * @returns {Promise<void>}
+     */
     async function fetchNumberFact() {
         let response = await fetch('http://numbersapi.com/random/trivia');
         response.text()
             .then(res => setNumber(res))
     }
+
+    /**
+     * On mounting fetches data and handle errors
+     */
     useEffect(() => {
-        fetchNumberFact().catch(err => console.log(err))
-        let arr = number.split(" ", 1)
-
-
+        fetchNumberFact()
+            .catch(err => console.log(err));
     }, []);
 
+    /**
+     * Render component
+     */
     return (
-        <div>
-            <span>{}</span>
-            <p> UUUU
-            </p>
-            <span>{number}</span>
+        <div className="row" style={{textAlign: "center", height: 80, justifyContent: "center"}}>
+            <span style={{color: "#e59400", fontSize: 28, padding: 10}}>{number}</span>
         </div>
     )
 };
