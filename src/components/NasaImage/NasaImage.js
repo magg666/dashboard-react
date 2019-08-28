ceccb6fccf3a660a51c0023796e9922bf09fdf33
magg666 <mw23127@gmail.com>
2019-08-28 22:43:16 +0200

    added backup image for failing getting it from Nasa

--- src/components/NasaImage/NasaImage.js
+++ src/components/NasaImage/NasaImage.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import dog from './dog.jpg'
 
 /**
  * Functional component displaying data from Nasa Api
@@ -10,32 +11,42 @@ export const NasaImage = () => {
     // Initial state
     let [image, setImage] = useState("");
     let [title, setTitle] = useState("");
+    let [type, setType] = useState("");
 
     // styling
     const styles = {
-        container: {
-            display: 'flex',
-            margin: 5
-        },
-        wrapper: {
-            justifyContent: "center",
-            border: '2px solid rgb(200, 200, 200)',
-            borderRadius: '10px',
-            height: '500px',
-            position: "relative"
-        },
-        img: {
-            width: '100%',
-            height: '100%',
-            objectFit: "contain"
-        },
-        titleImg: {
-            position: "absolute",
-            top: '8px',
-            right: '16px',
-            color: "white"
+            container: {
+                display: 'flex',
+                margin: 5
+            },
+            wrapper: {
+                justifyContent: "center",
+                border: '2px solid rgb(200, 200, 200)',
+                borderRadius: '10px',
+                height: '500px',
+                position: "relative"
+            },
+            img: {
+                width: '100%',
+                height: '100%',
+                objectFit: "contain"
+            },
+            iframe: {
+                width: 700,
+                height: '100%',
+                objectFit: "cover"
+            },
+            titleImg: {
+                position: "absolute",
+                top:
+                    '8px',
+                right:
+                    '16px',
+                color:
+                    "white"
+            }
         }
-    };
+    ;
 
     /**
      * Fetching data from Nasa Api
@@ -49,7 +60,8 @@ export const NasaImage = () => {
         response.json()
             .then(res => {
                 setImage(res["url"]);
-                setTitle(res["title"])
+                setTitle(res["title"]);
+                setType(res["media_type"])
             })
     }
 
@@ -58,12 +70,23 @@ export const NasaImage = () => {
      * Second parameter [] prevents rendering non-stop
      */
     useEffect(() => {
-        fetchNasaData().catch(err => console.log(err));
+        fetchNasaData().catch(err => console.log(err))
+
+
     }, []);
 
+
     // renders widget
-    // todo - get sample image if error occurs
-    return (
+    if (type === "video") { // nasa sometimes adds video from youtube
+        return (
+            <div style={styles.container}>
+                <div style={styles.wrapper}>
+                    <iframe title={"video"} style={styles.iframe} src={image + '&playlist=""&loop=1&autoplay=1&mute=1'}/>
+                </div>
+            </div>
+        )
+    } else if(type === "image"){
+        return (
         <div style={styles.container}>
             <div style={styles.wrapper}>
                 <img alt={"Nasa of day"} style={styles.img}
@@ -72,4 +95,16 @@ export const NasaImage = () => {
             </div>
         </div>
     )
+    }
+    // backup image
+    else {
+        return (
+            <div style={styles.container}>
+                <div style={styles.wrapper}>
+                    <img src={dog} alt={'dog'} style={styles.img}/>
+                    <div className="title" style={styles.titleImg}>Awesome dog from Nasa</div>
+                </div>
+            </div>
+            )
+    }
 };
