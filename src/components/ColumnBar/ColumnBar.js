b5864e5b5ea4713101af160dbb61cec2c3d35376
magg666 <mw23127@gmail.com>
2019-08-30 22:23:54 +0200

    added Column bar functionality added documentation

--- src/components/ColumnBar/ColumnBar.js
+++ src/components/ColumnBar/ColumnBar.js
@@ -1,90 +1,36 @@
 import React from 'react';
 import Chart from 'react-apexcharts'
 
+/**
+ * Column graph for total projects statistic in each module.
+ * Columns - additions and deletions of line of code
+ * Line - commits
+ * From apex-charts
+ * Documentation: https://apexcharts.com/react-chart-demos/mixed-charts/line-column/
+ */
 
 export default class BarChart extends React.Component {
-
-    getData(json, firstParam, secondParam) {
-        if (Array.isArray(json)) {
-            return json.map(obj => {
-                return obj[firstParam].map(pr => {
-                    return pr[secondParam]
-                })
-            })[0]
-        }
-    }
-
-    data = [{
-        "module": "Advanced Module",
-        "projects": [
-            {
-                "project": "Dashboard Api",
-                "total": [
-                    {
-                        "commits": 29,
-                        "additions": 267,
-                        "deletions": 230
-                    }
-                ]
-            },
-            {
-                "project": "Medicalspring",
-                "total": [
-                    {
-                        "commits": 6,
-                        "additions": 781,
-                        "deletions": 34
-                    }
-                ]
-            },
-            {
-                "project": "Dashboard Django",
-                "total": [
-                    {
-                        "commits": 14,
-                        "additions": 866,
-                        "deletions": 13
-                    }
-                ]
-            },
-            {
-                "project": "Smartest",
-                "total": [
-                    {
-                        "commits": 5,
-                        "additions": 723,
-                        "deletions": 2
-                    }
-                ]
-            }
-        ]
-    }];
-
-
     constructor(props) {
         super(props);
 
         this.state = {
-
             series: [
-
                 {
-                    name: 'Column A',
+                    name: 'Deletions',
                     type: 'column',
-                    data: [21.1, 23, 33.1, 234, 44.1, 44.9, 1156.5, 58.5]
+                    data: this.props['deletions']
                 },
                 {
-                    name: "Column B",
+                    name: "Additions",
                     type: 'column',
-                    data: [2781, 19, 27, 1426, 534, 135, 40, 38]
+                    data: this.props['additions']
                 },
                 {
-                    name: "Line C",
+                    name: "Commits",
                     type: 'line',
-                    data: [1.4, 29, 2.5, 1.5, 18, 2.8, 3.8, 49]
+                    data: this.props['commits']
                 },
             ],
-
 
             options: {
                 chart: {
@@ -102,12 +48,11 @@ export default class BarChart extends React.Component {
                         colors: ['#000']
                     },
                 },
-                colors: ['#99C2A2', '#C5EDAC', '#66C7F4'],
+                colors: ['#c249bf', '#edd089', '#66c7f4'],
 
                 stroke: {
                     width: [4, 4, 4]
                 },
-
 
                 markers: {
                     size: 6,
@@ -127,11 +72,11 @@ export default class BarChart extends React.Component {
                     }
                 },
                 xaxis: {
-                    categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016]
+                    categories: this.props['projectsTitles']
                 },
                 yaxis: [
                     {
-                        seriesName: 'Column A',
+                        seriesName: 'Deletions',
                         axisTicks: {
                             show: true
                         },
@@ -139,15 +84,15 @@ export default class BarChart extends React.Component {
                             show: true,
                         },
                         title: {
-                            text: "Columns"
+                            text: "Changes of code's lines"
                         }
                     },
                     {
-                        seriesName: 'Column A',
+                        seriesName: 'Additions',
                         show: false
                     }, {
                         opposite: true,
-                        seriesName: 'Line C',
+                        seriesName: 'Commits',
                         axisTicks: {
                             show: true
                         },
@@ -155,7 +100,7 @@ export default class BarChart extends React.Component {
                             show: true,
                         },
                         title: {
-                            text: "Line"
+                            text: "Commits"
                         }
                     }
                 ],
@@ -176,12 +121,10 @@ export default class BarChart extends React.Component {
 
     render() {
         return (
-
             <div id="chart">
-                <Chart options={this.state.options} series={this.state.series} type="bar" height="350"/>
-
+                <Chart options={this.state.options} series={this.state.series} type="bar"
+                       height={(window.innerHeight - 120)}/>
             </div>
-
         )
     }
 }
